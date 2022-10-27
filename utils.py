53904ad66e8f894421a768e05769ad95703aa515
prem1708 <89617835+prem1708@users.noreply.github.com>
2022-10-27 08:23:14 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import streamlit as st
 import whisper
 
 
-
 SAMPLES = {
     "DALL·E 2 Explained by OpenAI": "https://www.youtube.com/watch?v=qTgPSKKjfVg",
     "Streamlit Shorts: How to make a select box by Streamlit": "https://www.youtube.com/watch?v=8-GavXeFlEA"
@@ -39,6 +38,7 @@ def _get_audio_from_youtube_url(url):
         os.makedirs('data')
     yt.streams.filter(only_audio=True).first().download(filename=os.path.join('data','audio.mp3'))
 
+
 def _whisper_result_to_srt(result):
     text = []
     for i,s in enumerate(result['segments']):
@@ -57,6 +57,7 @@ def _whisper_result_to_srt(result):
         text.append(s['text'].strip() + "\n")
             
     return "\n".join(text)
+
 
 @st.experimental_memo(show_spinner=False, max_entries=1)
 def transcribe_youtube_video(_model, url):
