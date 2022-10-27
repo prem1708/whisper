f53e4a0e582dd95f8ef065f8fe14dd008fe983d4
prem1708 <89617835+prem1708@users.noreply.github.com>
2022-10-27 08:12:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -39,7 +39,24 @@ def _get_audio_from_youtube_url(url):
         os.makedirs('data')
     yt.streams.filter(only_audio=True).first().download(filename=os.path.join('data','audio.mp3'))
 
+def _whisper_result_to_srt(result):
+    text = []
+    for i,s in enumerate(result['segments']):
+        text.append(str(i+1))
 
+        time_start = s['start']
+        hours, minutes, seconds = int(time_start/3600), (time_start/60) % 60, (time_start) % 60
+        timestamp_start = "%02d:%02d:%06.3f" % (hours, minutes, seconds)
+        timestamp_start = timestamp_start.replace('.',',')     
+        time_end = s['end']
+        hours, minutes, seconds = int(time_end/3600), (time_end/60) % 60, (time_end) % 60
+        timestamp_end = "%02d:%02d:%06.3f" % (hours, minutes, seconds)
+        timestamp_end = timestamp_end.replace('.',',')        
+        text.append(timestamp_start + " --> " + timestamp_end)
+
+        text.append(s['text'].strip() + "\n")
+            
+    return "\n".join(text)
 
 @st.experimental_memo(show_spinner=False, max_entries=1)
 def transcribe_youtube_video(_model, url):
